13590788cd5659aef9d0a707a6c8fa124e78fe26
Antoine Frénoy <afrenoy@gmx.com>
2019-02-18 16:10:43 +0100

    Finish launch_tars.sh

--- launch_tars.sh
+++ launch_tars.sh
@@ -4,5 +4,5 @@ name=$(basename $1 .fasta)
 ./split_genome.sh $1 $name
 
 for f in ${name}/*.fasta; do
-  echo sbatch --qos=normal --mem-per-cpu=4096 --output="slurm-${name}-$(basename $f .fasta)-%j.out" --wrap="ribosor.py $f"
+  sbatch --qos=normal --mem-per-cpu=4096 --output="slurm-${name}-$(basename $f .fasta)-%j.out" --wrap="./ribosor.py $f 2019-02-16"
 done
