80a5195f220496420cfe97090c25cbaf532cff5c
Antoine Frénoy <afrenoy@gmx.com>
2019-03-11 16:45:51 +0100

    launch_all.sh: a script to launch the RiBoSor on a set of full genomes

--- launch_tars.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-name=$(basename $1 .fasta)
-./split_genome.sh $1 $name
-
-for f in ${name}/*.fasta; do
-  sbatch --qos=normal --mem-per-cpu=4096 --output="slurm-${name}-$(basename $f .fasta)-%j.out" --wrap="./ribosor.py $f 2019-02-16"
-done
